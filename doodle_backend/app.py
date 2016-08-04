c111177f8abd14ba0b7440d72ee59bbbf2986bbe
Evgeny Tataurov <tatauroff@gmail.com>
2016-08-05 00:11:17 +0200

    more things done

--- doodle_backend/app.py
+++ doodle_backend/app.py
@@ -1,10 +1,22 @@
 from gevent import monkey
 monkey.patch_all()
 import os
+import uuid
 import flask
+import datetime
+
+from redis import Redis
 from .tasks import *
 
+from flask_oauthlib.provider import OAuth2Provider
+
+redis_host = os.environ.get("REDIS_HOST", "localhost")
+redis_client = Redis(redis_host)
+
 app = flask.Flask(__name__)
+app.config["OAUTH2_PROVIDER_TOKEN_EXPIRES_IN"] = 36000000000
+oauth = OAuth2Provider(app)
+
 cwd = os.path.abspath(os.path.dirname(__file__))
 SAMPLES_FOLDER = os.path.join(cwd, "online_doodle_files")
 
@@ -12,6 +24,8 @@ SAMPLES_FOLDER = os.path.join(cwd, "online_doodle_files")
 
 # we need to keep styles somewhere
 # in local json file?
+
+CLIENT_ID = os.environ.get("CLIENT_ID") or "some_client_id"
 
 STYLES = {
     "monet": {
@@ -38,7 +52,69 @@ STYLES = {
 }
 
 
-@app.route("/list")
+class Client:
+    def __init__(self):
+        self.client_id = CLIENT_ID
+        self.user = ""
+        self.default_redirect_uri = ""
+        self.allowed_grant_types = ["client_credentials"]
+        self.default_scopes = ["default"]
+
+client = Client()
+
+
+@oauth.clientgetter
+def load_client(client_id):
+    return client
+
+
+@oauth.grantgetter
+def load_grant(client_id, code):
+    return True
+
+
+@oauth.grantsetter
+def save_grant(client_id, code, request, *args, **kwargs):
+    return True
+
+
+class Token:
+    def __init__(self, client_id, expires):
+        self.expires = expires
+        self.scopes = ["default"]
+        self.user = None
+
+
+@oauth.tokengetter
+def load_token(access_token=None, refresh_token=None):
+    print("getting token")
+    print(access_token)
+    result = redis_client.hgetall(access_token)
+    print(result)
+    if result:
+        expires = datetime.datetime.strptime(result[b"expires"].decode(), "%Y-%m-%d %H:%M:%S.%f")
+        client_id = result[b"client_id"]
+        return Token(client_id=client_id, expires=expires)
+    return None
+
+
+@oauth.tokensetter
+def save_token(token, request, *args, **kwargs):
+    expires_in = token.get('expires_in')
+    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+    access_token = token.get("access_token")
+    client_id = request.client.client_id
+    redis_client.hmset(access_token, {"client_id": client_id, "expires": expires})
+    return Token(client_id=client_id, expires=expires)
+
+
+@app.route('/oauth/token')
+@oauth.token_handler
+def access_token():
+    return None
+
+@app.route("/api/styles")
+@oauth.require_oauth('default')
 def list_styles():
     result = []
     for key, value in STYLES.items():
@@ -56,7 +132,23 @@ def original_picture(style):
                                      STYLES[style]["original"])
 
 
-@app.route('/task')
-def perform_taks():
-    result = add.delay(4, 4)
-    return str(result.get())
+@app.route('/result/<filename>')
+def result(filename):
+    return flask.send_from_directory(SAMPLES_FOLDER, filename)
+
+
+@app.route("/api/process_image", methods=["POST"])
+@oauth.require_oauth('default')
+def process_handler():
+    if flask.request.method == 'POST':
+        file = flask.request.files['image']
+        style = flask.request.form.get("style")
+        if file:
+            uid = str(uuid.uuid4())
+            filename = uid + "_mask.png"
+            file.save(os.path.join(SAMPLES_FOLDER, filename))
+            style_data = STYLES[style]
+            result = process_image.delay(uid, style_data["colors"], style_data["model"])
+            result.get()
+            return flask.jsonify(uid=uid, result_url=flask.url_for('result', filename="{}.png".format(uid)))
+    flask.abort(400)
--- doodle_backend/tasks.py
+++ doodle_backend/tasks.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 import time
+import subprocess
 
 url = os.environ.get("RABBIT_URL") or 'amqp://guest@localhost//'
 
@@ -11,7 +12,27 @@ app.conf.CELERY_ACCEPT_CONTENT = ['json']
 app.conf.CELERY_RESULT_BACKEND = url
 
 
+cwd = ""
+MOUNT_FOLDER = ""
+
+
 @app.task
 def add(x, y):
     time.sleep(10)
     return x + y
+
+
+@app.task
+def process_image(item_id, colors, model):
+    subprocess.call(["nvidia-docker", "run", "-v",
+                     "/data/repos/online-neural-doodle/:/root/data",
+                     "-v",
+                     "/data/repos/doodle_web/online_doodle_files:/root/doodle/",
+                     "conv_image", "apply.py",
+                     "--colors", "/root/data/" + colors,
+                     "--target_mask",
+                     os.path.join(MOUNT_FOLDER, "{}_mask.png".format(item_id)),
+                     "--model", "/root/data/" + model,
+                     "--out_path",
+                     os.path.join(MOUNT_FOLDER, "{}.png".format(item_id))],
+                    cwd=os.path.join(cwd, os.pardir))
